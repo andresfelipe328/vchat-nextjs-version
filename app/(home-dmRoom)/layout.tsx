590f3f2a7d7a5a31de950f0fe24b5422c24e0528
Andres Romero <andresfelipe328@hotmail.com>
2023-06-09 11:51:22 -0700

    remove server actions

--- app/(home-dmRoom)/layout.tsx
+++ app/(home-dmRoom)/layout.tsx
@@ -1,12 +1,4 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  // Server Action: Add Friend ========================================================================================
-  const createDmRoom = async (username: string) => {
-    "use server";
-
-    console.log("Creating dm room...");
-  };
-  // ==================================================================================================================
-
   return (
     <>
       <p>mngmt dmRoom bar</p>
