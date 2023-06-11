727533e7bc2ecbddc2482dcf538a3acfda7409d7
Andres Romero <andresfelipe328@hotmail.com>
2023-06-11 07:57:12 -0700

    basic template without chat component

--- components/homePage/dmRoomMgmntBar/CreateDmRoom.tsx
+++ components/homePage/dmRoomMgmntBar/CreateDmRoom.tsx
@@ -1,15 +1,11 @@
 import CreateDmRoomPopup from "@/components/popups/CreateDmRoomPopup";
 
-type Props = {
-  createDmRoom?: Function;
-};
-
-const CreateDmRoom = ({ createDmRoom }: Props) => {
+const CreateDmRoom = () => {
   return (
     <div className="p-2 flex items-center justify-between z-20">
       <h3>DM Room</h3>
 
-      <CreateDmRoomPopup createDmRoom={createDmRoom} />
+      <CreateDmRoomPopup />
     </div>
   );
 };
